8730cf2b85f49bd635dd7c1a340e6304432ef73e
puridev1155 <puridev1155@gmail.com>
2023-01-05 07:55:07 +0900

    tailwind config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,27 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Nunito", ...defaultTheme.fontFamily.sans],
       },
     },
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+      'md': '860px',
+      // => @media (min-width: 960px) { ... }
+      'lg': '1200px',
+      // => @media (min-width: 1440px) { ... }
+      'xl': '1440px'
+    }
   },
   plugins: [require("@tailwindcss/forms")],
 };
